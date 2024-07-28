6e4c961ff534f301c99fb95ad898202f78ef2aa1
Fardeen <fardeenclan@gmail.com>
2024-07-28 20:25:00 +0530

    init and added home page with document formation

---
+++ app/(root)/page.tsx
@@ -0,0 +1,80 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import { getDocuments } from "@/lib/actions/room.actions";
+import Link from "next/link";
+import { dateConverter } from "@/lib/utils";
+
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+  const docs = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {docs.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All Document</h3>
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <ul className="document-ul">
+            {docs.data.map((doc: any) => (
+              <li key={doc.id} className="document-list-item">
+                <Link
+                  href={`/documents/${doc.id}`}
+                  className="flex flex-1 items-center gap-4"
+                >
+                  <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                    <Image
+                      src={"/assets/icons/doc.svg"}
+                      alt="file"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className="space-y-1">
+                    <p className="line-clamp-1 text-lg">{doc.metadata.title}</p>
+                    <p className="text-sm font-light text-blue-100">
+                      Created about {dateConverter(doc.createdAt)}
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src={"/assets/icons/doc.svg"}
+            alt="docs"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default Home;
---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,116 @@
+"use client";
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import Header from "./Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Editor } from "./editor/Editor";
+import ActiveCollaborators from "./ActiveCollaborators";
+import Loader from "./Loader";
+import { useEffect, useRef, useState } from "react";
+import { Input } from "./ui/input";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { updateDocument } from "@/lib/actions/room.actions";
+
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
+  const [editing, setediting] = useState(false);
+  const [loading, setloading] = useState(false);
+  const [documentTitle, setdocumentTitle] = useState(roomMetadata.title);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+  const updateTitleHander = async (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === "Enter") {
+      setloading(true);
+      try {
+        if (documentTitle !== roomMetadata.title) {
+          const updatedDocument = await updateDocument(roomId, documentTitle);
+          if (updatedDocument) setediting(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  const currentUserType = "editor";
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setediting(false);
+        updateDocument(roomId, documentTitle);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [roomId, documentTitle]);
+  useEffect(() => {
+    if (editing && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editing]);
+  return (
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<Loader />}>
+        <div className="collaborative-room">
+          <Header>
+            <div
+              ref={containerRef}
+              className="flex w-fit items-center justify-center gap-2"
+            >
+              {editing && !loading ? (
+                <Input
+                  type="text"
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder="Enter title"
+                  onChange={(e) => setdocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHander}
+                  disabled={!editing}
+                  className="document-title-input"
+                />
+              ) : (
+                <>
+                  <p className="document-title">{documentTitle}</p>
+                </>
+              )}
+              {currentUserType === "editor" && !editing && (
+                <Image
+                  src={"/assets/icons/edit.svg"}
+                  alt="edit"
+                  width={24}
+                  height={24}
+                  onClick={() => setediting(true)}
+                  className="cursor-pointer"
+                />
+              )}
+              {currentUserType !== "editor" && !editing && (
+                <p className="view-only-tag">View onlt</p>
+              )}
+              {loading && <p className="text-sm text-gray-400">Saving... </p>}
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
